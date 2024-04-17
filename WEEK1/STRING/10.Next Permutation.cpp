a71d541d125187c6f01064a811361a520c7d9813
Anusha D K <122234698+anushadk13@users.noreply.github.com>
2024-04-17 23:03:07 +0530

    Update 10.Next Permutation.cpp

--- WEEK1/STRING/10.Next Permutation.cpp
+++ WEEK1/STRING/10.Next Permutation.cpp
@@ -1,2 +1,54 @@
 Next Permutation
 link:https://www.geeksforgeeks.org/problems/next-permutation5226/1
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextPermutation(int N, vector<int> arr) {
+        int i = arr.size() - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            int j = arr.size() - 1;
+            while (j >= 0 && arr[j] <= arr[i]) {
+                j--;
+            }
+            swap(arr, i, j);
+        }
+        
+        reverse(arr, i + 1);
+        return arr; // Added return statement
+    }
+    
+    void swap(vector<int>& arr, int i, int j) { // Pass vector by reference
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    void reverse(vector<int>& arr, int start) { // Corrected signature
+        int end = arr.size() - 1; // Initialize end index
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+};
+
+
+or
+
+
+class Solution{
+public:
+    vector<int> nextPermutation(int N, vector<int> arr){
+        // code here
+        next_permutation(arr.begin(),arr.end());
+        return arr;
+    }
+};
