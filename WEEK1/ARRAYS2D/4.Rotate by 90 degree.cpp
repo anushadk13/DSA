1e1ce3eea56aa58741b3e7a3429b816fbc082a76
Anusha D K <122234698+anushadk13@users.noreply.github.com>
2024-04-18 23:27:09 +0530

    Update 4.Rotate by 90 degree.cpp

--- WEEK1/ARRAYS2D/4.Rotate by 90 degree.cpp
+++ WEEK1/ARRAYS2D/4.Rotate by 90 degree.cpp
@@ -1,2 +1,52 @@
 Rotate by 90 degree
 link:https://www.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/1
+
+run time:1ms
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+
+        }
+         for(int i=0;i<n;i++)
+         reverse(matrix[i].begin(),matrix[i].end());
+        
+    }
+};
+
+
+run time:4ms
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix )
+    {
+        int n= matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        for(int i=0 ;i<n;i++)
+        {
+            int low=0 ,high = n-1;
+            while(low<high)
+            {
+                swap(matrix[i][low],matrix[i][high]);
+                low++;
+                high--;
+            }
+        }
+       
+    }
+};
